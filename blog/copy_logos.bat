9b62a7803110d0e2056235c3261d38ff2de20d09
bezialaurent-lang <bezialaurent@gmail.com>
2026-07-10 21:10:43 +0200

    Ajout du soutien de Carole Delga (portrait et lettre de soutien)

--- blog/copy_logos.bat
+++ blog/copy_logos.bat
@@ -4,5 +4,7 @@ copy /Y "..\soutien syndical\afash.jpg" "photos\syndicats\afash.jpg"
 copy /Y "..\soutien syndical\logo-chru-tours.png" "photos\syndicats\logo-chru-tours.png"
 copy /Y "..\soutien syndical\chu-angers-300x185.png" "photos\syndicats\chu-angers-300x185.png"
 copy /Y "..\soutien syndical\CHU_de_Montpellier_(logo).svg.png" "photos\syndicats\CHU_de_Montpellier_(logo).svg.png"
+copy /Y "..\soutien syndical\portrait carole delga.png" "photos\syndicats\portrait carole delga.png"
+copy /Y "..\soutien syndical\lettre delga.jpeg" "media\lettre delga.jpeg"
 echo Copie terminee !
 pause
--- blog/push.bat
+++ blog/push.bat
@@ -4,11 +4,13 @@ copy /Y "..\soutien syndical\afash.jpg" "photos\syndicats\afash.jpg"
 copy /Y "..\soutien syndical\logo-chru-tours.png" "photos\syndicats\logo-chru-tours.png"
 copy /Y "..\soutien syndical\chu-angers-300x185.png" "photos\syndicats\chu-angers-300x185.png"
 copy /Y "..\soutien syndical\CHU_de_Montpellier_(logo).svg.png" "photos\syndicats\CHU_de_Montpellier_(logo).svg.png"
+copy /Y "..\soutien syndical\portrait carole delga.png" "photos\syndicats\portrait carole delga.png"
+copy /Y "..\soutien syndical\lettre delga.jpeg" "media\lettre delga.jpeg"
 
 echo Ajout des fichiers modifies...
 git add .
 echo Creation du commit...
-git commit -m "Ajout communique mobilisation 19 juin 2026, compteur 401241, et equilibrage des logos de soutiens"
+git commit -m "Ajout du soutien de Carole Delga (portrait et lettre de soutien)"
 echo Pousse vers le repertoire distant...
 git push
 echo.
